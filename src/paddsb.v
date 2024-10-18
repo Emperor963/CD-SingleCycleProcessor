7f9dceeeecdb285d555bc955535b0b347a4b3675
Anna Kallsen <akallsen@wisc.edu>
2024-10-18 00:59:26 -0500

    adds paddsb

---
+++ src/paddsb.v
@@ -0,0 +1,25 @@
+module paddsb (
+    input [15:0] A;
+    input [15:0] B;
+    input ovfl;
+
+    output []
+);
+
+wire [3:0]A_a; //15-12
+wire [3:0]A_b; //11-9
+wire [3:0]A_c; //8-4
+wire [3:0]A_d; //3-0
+
+wire [3:0]B_a; //15-12
+wire [3:0]B_b; //11-9
+wire [3:0]B_c; //8-4
+wire [3:0]B_d; //3-0
+
+
+
+
+assign  = (A_a[3])
+
+
+endmodule 
