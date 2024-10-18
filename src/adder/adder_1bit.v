42f5ac98145ed3a310032111e487971d11b51912
Anna Kallsen <akallsen@wisc.edu>
2024-10-17 22:39:08 -0500

    added adder 4 bit

--- src/adder/adder_1bit.v
+++ src/adder/adder_1bit.v
@@ -1,13 +1,27 @@
-module adder_1bit;
 
-    reg A;
-    reg B; 
-    reg C;
 
-    wire sum;
-    wire overflow;
+module adder_1bit {
 
+    input [3:0]A,
+    input [3:0]B,
+    input  C,
+    output [3:0]Sum,
+    output Cout,
+    output Ovfl }
     
+
+
+
+    wire G;
+    wire P;
+
+    assign G = (A & B) ? 1b'1:1b'0;
+    assign P = (A | B) ? 1b'1:1b'0;
+
+
+
+
+
 
 
 
