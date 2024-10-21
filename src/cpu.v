4df7588b52ad1bb67b88ff0105b54d890b2fba4f
Emperor963 <bhattachar37@wisc.edu>
2024-10-21 08:42:28 -0500

    Changes to cpu added

---
+++ src/cpu.v
@@ -0,0 +1,10 @@
+module cpu(
+    input clk,
+    input rst,
+
+    output hlt,
+    output [15:0] pc
+);
+
+
+endmodule
---
+++ src/flag_reg.v
@@ -0,0 +1,13 @@
+module flag_reg(
+    input clk, 
+    input rst, 
+    input [2:0] wr
+    input [2:0] in, 
+    output [2:0] out,
+);
+
+dff u1(.q(out[0]), .d(in[0]), .wen(wr[0]), .clk(clk), .rst(rst));
+dff u2(.q(out[1]), .d(out[1]), .wen(wr[1]), .clk(clk), .rst(rst));
+dff u3(.q(out[2]), .d(out[2]), .wen(wr[2]), .clk(clk), .rst(rst));
+
+endmodule
