36b345633feba395a2c9596af4de97958b39d8c0
Emperor963 <bhattachar37@wisc.edu>
2024-10-25 15:14:02 -0500

    Minor changes

---
+++ src/logunits/ALU.v
@@ -0,0 +1,17 @@
+module ALU(
+    input [15:0] In1,
+    input [15:0] In2,
+    input [2:0] ALUOp,
+
+    output [2:0] FLAG,
+    output [15:0] ALUOut
+);
+
+wire [15:0] sum;
+wire [15:0] difference;
+wire [15:0] paddsb;
+
+
+
+
+endmodule
