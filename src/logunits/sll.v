cc875acb615c36281bb63cf51f6f6c77101ce422
Emperor963 <bhattachar37@wisc.edu>
2024-10-25 12:33:04 -0500

    Bug fix

--- src/logunits/sll.v
+++ src/logunits/sll.v
@@ -8,11 +8,11 @@ module sll(
 wire[15:0] At1, At2, At4, At8; //temporary storages for constant shifts
 
 assign At1 = shamt[0] ? (Ain << 1) : Ain;
-assign At2 = shamt[0] ? (At1 << 2) : At1;
+assign At2 = shamt[1] ? (At1 << 2) : At1;
 assign At4 = shamt[2] ? (At2 << 4) : At2;
 assign At8 = shamt[3] ? (At4 << 8) : At4;
 
-assign Aout = shamt[4] ? 16'b0 ; At8;
+assign Aout = shamt[4] ? 16'b0 : At8;
 
 
 endmodule
