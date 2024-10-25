e626f7a0ba8eb6534cc0fd48c4669eecf3aed52e
Anna Kallsen <akallsen@wisc.edu>
2024-10-25 17:40:53 -0500

    control unit

--- src/cpu_control.v
+++ src/cpu_control.v
@@ -1,5 +1,6 @@
 module cpu_control(
     input [3:0]control,
+
     output RegRead,
     output MemRead,
     output [1:0]MemtoReg,
@@ -7,10 +8,8 @@ module cpu_control(
     output [2:0]ALUOp,
     output ALUsrc,
     output RegWrite,
-    output PCSource,
-    output PCW,
-    output LH
-); 
+    output [1:0]PCSour,
+    output LH ); 
 
     /* result = 
                 RegRead
@@ -23,80 +22,82 @@ module cpu_control(
 
     */
 
-    wire [6:0]result;
-    wire [2:0]ALU;
-    wire [1:0]MR;
+    reg [5:0]result;
+    reg [2:0]ALU;
+    reg [1:0]MR;
+    reg [1:0]PCSource;
 
     // Assign bits of result to output ports
-    assign RegRead  = result[6];  // Bit 6 of result
-    assign MemRead  = result[5];  // Bit 5 of result
-    assign MemWrite = result[4];  // Bit 4 of result
+    assign RegRead  = result[0];  // Bit 0 of result
+    assign MemRead  = result[1];  // Bit 1 of result
+    assign MemWrite = result[2];  // Bit 2 of result
     assign ALUsrc   = result[3];  // Bit 3 of result
-    assign RegWrite = result[2];  // Bit 2 of result
-    assign PCSource = result[1];  // Bit 1 of result
-    assign PCW      = result[0];  // Bit 0 of result
-    assign LH       = result[7];  // Bit 7 of result
+    assign RegWrite = result[4];  // Bit 4 of result
+    assign LH       = result[5];  // Bit 5 of result
 
     assign ALUOp = ALU;
 
     assign MemtoReg = MR;
+    
+    assign PCSour = PCSource;
 
 
 always @(*) begin
     case (control)
             4'b0000: begin      // ADD
-            result = 8'b1000100; ALU = 3'b 000; MR = 2'b10;
+            result = 6'b10001x; ALU = 3'b000; MR = 2'b10; PCSource = 2'b00;
             end
             4'b0001: begin
-            result = 8'b1000100; ALU = 3'b001;  MR= 2'b10;       // SUB
+            result = 6'b10001x; ALU = 3'b001;  MR= 2'b10;  PCSource = 2'b00;     // SUB
             end
             4'b0010: begin
-            result = 8'b1000100; ALU = 3'b010;  MR= 2'b10;      // XOR
+            result = 6'b10001x; ALU = 3'b010;  MR= 2'b10;  PCSource = 2'b00;    // XOR
             end
             4'b0011: begin
-            result = 8'b1000100; ALU = 3'b011;  MR= 2'b10;      // RED 
+            result = 6'b10001x; ALU = 3'b011;  MR= 2'b10;  PCSource = 2'b00;    // RED 
             end
             4'b0100: begin
-            result = 8'b1000100; ALU = 3'b100;  MR= 2'b10;       // SLL
+            result = 6'b10011x; ALU = 3'b100;  MR= 2'b10;   PCSource = 2'b00;    // SLL
             end
             4'b0101: begin
-            result = 8'b1000100; ALU = 3'b101;  MR= 2'b10;      // SRA 
+            result = 6'b10011x; ALU = 3'b101;  MR= 2'b10;  PCSource = 2'b00;    // SRA 
             end
             4'b0110: begin
-            result = 8'b1000100; ALU = 3'b110;  MR= 2'b10;      // ROR (Rotate Right)
+            result = 6'b10011x; ALU = 3'b110;  MR= 2'b10;  PCSource = 2'b00;    // ROR (Rotate Right)
             end
             4'b0111: begin
-            result = 8'b1000100; ALU = 3'b111;  MR= 2'b10;       // PADDSB (Padding Signed Bytes)
+            result = 6'b10001x; ALU = 3'b111;  MR= 2'b10;  PCSource = 2'b00;     // PADDSB (Padding Signed Bytes)
             end
 
             4'b1000: begin                         
-            result = 8'b1101100; ALU = 3'b000;  MR= 2'b11;        //LW 
+            result = 6'b11011x; ALU = 3'b000;  MR= 2'b11;   PCSource = 2'b00;     //LW 
             end
             4'b1001: begin                        
-            result = 8'b1011100; ALU = 3'b000;  MR= 2'b00;       // SW
+            result = 6'b10110x; ALU = 3'b000;  MR= 2'bxx;  PCSource = 2'b00;     // SW
             end
             4'b1010:  begin                       
-            result = 8'b0001100; ALU = 3'b010;  MR= 2'b01;       // LLB
+            result = 6'b000x10; ALU = 3'bxxx;  MR= 2'b01;  PCSource = 2'b00;     // LLB
             end
             4'b1011:  begin                      
-            result = 8'b0001100; ALU = 3'b010;  MR= 2'b01;        // LHB
+            result = 6'b000x11; ALU = 3'bxxx;  MR= 2'b01;  PCSource = 2'b00;      // LHB
             end
             4'b1100:  begin                      
-            result = 8'b0001010; ALU = 3'b010;  MR= 2'b00;         // B
+            result = 6'bx00x0x; ALU = 3'bxxx;  MR= 2'bxx;  PCSource = 2'b11;       // B
             end
             4'b1101:  begin                       
-            result = 8'b1000010; ALU = 3'b010;  MR= 2'b00;         // BR
+            result = 6'b100x0x; ALU = 3'bxxx;  MR= 2'bxx;  PCSource = 2'b01;       // BR
             end
             4'b1110:  begin                     
-            result = 8'b0000101; ALU = 3'b010;  MR= 2'b00;          //PCS
+            result = 6'bx00x1x; ALU = 3'bxxx;  MR= 2'b00;  PCSource = 2'b00;        //PCS
             end
             4'b1111:  begin                       
-            result = 8'b0000000; ALU = 3'b010;  MR= 2'b00;          //HLT
+            result = 6'bxxxxxx; ALU = 3'bxxx;  MR= 2'bxx;  PCSource = 2'b01;        //HLT
             end
             default: begin  // Default case to handle unknown opcodes
-                result = 7'b0000000;
+                result = 6'b000000;
                 ALU = 3'b000;
                 MR = 2'b00;
+                PCSource = 2'b00;
             end
     endcase
 
